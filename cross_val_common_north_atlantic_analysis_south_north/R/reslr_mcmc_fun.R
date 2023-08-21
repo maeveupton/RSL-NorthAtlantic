7877ee357d9c8e032641f04308e74f65e3204fb9
maeveupton <uptonmaeve010@gmail.com>
2023-08-21 18:13:57 +0100

    update

---
+++ cross_val_common_north_atlantic_analysis_south_north/R/reslr_mcmc_fun.R
@@ -0,0 +1,312 @@
+#' Run Markov chain Monte Carlo (MCMC) function
+reslr_mcmc_fun <- function(data, data_grid,
+                           n_iterations = 5000,
+                           n_burnin = 1000,
+                           n_thin = 4,
+                           n_chains = 3,
+                           spline_nseg_t = 8,
+                           spline_nseg_st = 6,
+                           spline_nseg_c = 20,
+                           CI = 0.95
+) {
+  
+  # Section: i.e. East West id set up-------
+  section_tibble <- tibble::tibble(
+    section = c("South", "North"),
+    section_index = c(1, 2)
+  )
+  
+  
+  # Input Data -------------
+  data <- data %>%
+    dplyr::mutate(Age = Age / 1000, Age_err = Age_err / 1000) %>%
+    mutate(section = factor(section,
+                            levels = c("South", "North")
+    ))
+  data <- left_join(data, section_tibble)
+  
+  # Input Data grid--------------
+  data_grid <- data_grid %>%
+    dplyr::mutate(Age = Age / 1000) %>%
+    mutate(section = factor(section,
+                            levels = c("South", "North")
+    ))
+  data_grid <- left_join(data_grid, section_tibble)
+  
+  
+  # Basis functions -----------------------------
+  spline_basis_fun_list <- spline_basis_fun(
+    data = data,
+    data_grid = data_grid,
+    spline_nseg_t = spline_nseg_t,
+    spline_nseg_st = spline_nseg_st,
+    spline_nseg_c = spline_nseg_c
+  )
+  
+  # JAGS file for Common Component----------------------------------------------------
+  common_jags_file <- "jags_models/common_model_ni_gam_decomp.jags"
+  
+  # JAGS data
+  jags_data <- list(
+    y = data$RSL,
+    y_err = data$RSL_err,
+    t = data$Age,
+    site = as.factor(data$SiteName),
+    n_sites = length(unique(data$SiteName)),
+    n_obs = nrow(data),
+    B_c = spline_basis_fun_list$B_c,
+    n_knots_c = ncol(spline_basis_fun_list$B_c),
+    linear_rate = data %>%
+      dplyr::group_by(SiteName) %>%
+      dplyr::slice(1) %>%
+      dplyr::select(linear_rate) %>%
+      dplyr::pull(),
+    linear_rate_err = data %>%
+      dplyr::group_by(SiteName) %>%
+      dplyr::slice(1) %>%
+      dplyr::select(linear_rate_err) %>%
+      dplyr::pull()
+  )
+  
+  # Parameters to save in JAGs
+  jags_pars <- c(
+    "mu_y",
+    "sigma_y",
+    "b_c",
+    "b_g",
+    #"h_z_x",
+    #"g_h_z_x",
+    #"g_z_x",
+    #"c",
+    "intercept",
+    "sigma_beta_h",
+    "sigma_beta_c",
+    "sigmasq_all"
+  )
+  
+  # Run JAGS------------------------
+  common_model_run <- suppressWarnings(R2jags::jags.parallel(
+    data = jags_data,
+    parameters.to.save = jags_pars,
+    model.file = common_jags_file,
+    n.iter = n_iterations,
+    n.burnin = n_burnin,
+    n.thin = n_thin,
+    n.chains = n_chains
+  ))
+  
+  data <- data %>% mutate(SiteName = as.factor(SiteName))
+  data_grid <- data_grid %>% mutate(SiteName = as.factor(SiteName))
+  
+  # Adding Noisy Input-------------------
+  update_input_df <- add_noisy_input(
+    data = data,
+    data_grid = data_grid,
+    model_run = common_model_run,
+    jags_data = jags_data,
+    spline_nseg_t = spline_nseg_t,
+    spline_nseg_st = spline_nseg_st,
+    spline_nseg_c = spline_nseg_c
+  )
+  data <- update_input_df$data
+  data_grid <- update_input_df$data_grid
+  
+  # JAGS file----------------------------------------------------
+  jags_file <- "jags_models/reg_model_ni_gam_decomp.jags"
+  
+  # JAGS data
+  jags_data <- list(
+    section = data$section_index,
+    section_obs_id = data$section_obs_id,
+    n_section = length(unique(data$section_index)),
+    b_c_value = common_model_run$BUGSoutput$median$b_c,
+    b_c_sd_value = common_model_run$BUGSoutput$sd$b_c,
+    h_value = common_model_run$BUGSoutput$median$intercept,
+    h_sd_value = common_model_run$BUGSoutput$sd$intercept,
+    y = data$RSL,
+    y_err = data$RSL_err,
+    t = data$Age,
+    site = as.factor(data$SiteName),
+    n_sites = length(unique(data$SiteName)),
+    n_obs = nrow(data),
+    B_r = spline_basis_fun_list$B_t,
+    n_knots_r = ncol(spline_basis_fun_list$B_t),
+    B_c = spline_basis_fun_list$B_c,
+    n_knots_c = ncol(spline_basis_fun_list$B_c),
+    linear_rate = data %>%
+      dplyr::group_by(SiteName) %>%
+      dplyr::slice(1) %>%
+      dplyr::select(linear_rate) %>%
+      dplyr::pull(),
+    linear_rate_err = data %>%
+      dplyr::group_by(SiteName) %>%
+      dplyr::slice(1) %>%
+      dplyr::select(linear_rate_err) %>%
+      dplyr::pull()
+  )
+  
+  # Parameters to save in JAGs
+  jags_pars <- c(
+    "mu_y",
+    "sigma_y",
+    "b_r",
+    #"h_z_x",
+    #"g_h_z_x",
+    #"g_z_x",
+    #"r",
+    #"r_deriv",
+    #"c",
+    #"c_deriv",
+    "b_c",
+    "intercept",
+    "sigma_beta_r",
+    "b_g",
+    "sigmasq_all"
+    #"diff_r",
+    #"diff_r_deriv"
+  )
+  
+  # Run JAGS------------------------
+  model_run <- suppressWarnings(R2jags::jags.parallel(
+    data = jags_data,
+    parameters.to.save = jags_pars,
+    model.file = jags_file,
+    n.iter = n_iterations,
+    n.burnin = n_burnin,
+    n.thin = n_thin,
+    n.chains = n_chains
+  ))
+  
+  
+  # Include Noise-----------------------
+  noisy_jags_file <- "jags_models/noisy_model_ni_gam_decomp.jags"
+  
+  # JAGS input data
+  jags_data <- list(
+    b_c_value = model_run$BUGSoutput$median$b_c,
+    b_c_sd_value = model_run$BUGSoutput$sd$b_c,
+    section = data$section_index,
+    n_section = length(unique(data$section_index)),
+    NI_var_term = data$NI_var_term,
+    NI_var_term_pred = data_grid$NI_var_grid_term,
+    b_r_value = model_run$BUGSoutput$median$b_r,
+    b_r_sd_value = model_run$BUGSoutput$sd$b_r,
+    h_value = model_run$BUGSoutput$median$intercept,
+    h_sd_value = model_run$BUGSoutput$sd$intercept,
+    y = data$RSL,
+    y_err = data$RSL_err,
+    y_err_pred = data_grid$RSL_err,
+    t = data$Age,
+    t_pred = data_grid$Age,
+    site = as.factor(data$SiteName),
+    site_pred = factor(data_grid$SiteName, levels = levels(as.factor(data$SiteName))),
+    n_sites = length(unique(data$SiteName)),
+    n_sites_pred = length(unique(data_grid$SiteName)),
+    section_pred = as.numeric(data_grid$section_index),
+    n_section_pred = length(unique(data_grid$section)),
+    n_pred = length(data_grid$Age),
+    n_obs = nrow(data),
+    B_c = spline_basis_fun_list$B_c,
+    B_c_deriv = spline_basis_fun_list$B_c_deriv,
+    B_c_pred = spline_basis_fun_list$B_c_pred,
+    B_c_pred_deriv = spline_basis_fun_list$B_c_pred_deriv,
+    n_knots_c = ncol(spline_basis_fun_list$B_c),
+    B_r = spline_basis_fun_list$B_t,
+    B_r_deriv = spline_basis_fun_list$B_t_deriv,
+    B_r_pred = spline_basis_fun_list$B_t_pred,
+    B_r_pred_deriv = spline_basis_fun_list$B_t_pred_deriv,
+    n_knots_r = ncol(spline_basis_fun_list$B_t),
+    B_st = spline_basis_fun_list$B_st,
+    B_st_pred = spline_basis_fun_list$B_st_pred,
+    B_st_deriv = spline_basis_fun_list$B_st_deriv,
+    B_st_deriv_pred = spline_basis_fun_list$B_st_deriv_pred,
+    n_knots_st = ncol(spline_basis_fun_list$B_st),
+    linear_rate = data %>%
+      dplyr::group_by(SiteName) %>%
+      dplyr::slice(1) %>%
+      dplyr::select(linear_rate) %>%
+      dplyr::pull(),
+    linear_rate_err = data %>%
+      dplyr::group_by(SiteName) %>%
+      dplyr::slice(1) %>%
+      dplyr::select(linear_rate_err) %>%
+      dplyr::pull()
+  )
+  
+  # Parameters to save in JAGs
+  jags_pars <- c(
+    "mu_y",
+    #"mu_deriv",
+    "mu_pred",
+    #"mu_pred_deriv",
+    "sigma_y",
+    #"sigma_beta_l",
+    #"sigmasq_all",
+    #"b_r",
+    #"b_st",
+    #"b_g",
+    #"b_c",
+    #"intercept",
+    #"h_z_x",
+    #"h_z_x_pred",
+    #"g_h_z_x",
+    #"g_h_z_x_pred",
+    ##"g_z_x",
+    #"g_z_x_deriv",
+    #"g_z_x_pred",
+    #"g_z_x_pred_deriv",
+    #"c",
+    #"c_deriv",
+    #"c_pred",
+    #"c_pred_deriv",
+    #"r",
+    #"r_deriv",
+    #"diff_r",
+    #"diff_r_deriv",
+    #"diff_r_pred",
+    #"diff_r_pred_deriv",
+    #"r_pred",
+    #"r_pred_deriv",
+    #"l",
+    #"l_deriv",
+    #"l_pred",
+    #"l_pred_deriv",
+    "residuals",
+    "sigmasq_all_pred",
+    "y_pred"
+  )
+  # Run JAGS--------------
+  noisy_model_run_output <-
+    suppressWarnings(R2jags::jags.parallel(
+      data = jags_data,
+      parameters.to.save = jags_pars,
+      model.file = noisy_jags_file,
+      n.iter = n_iterations,
+      n.burnin = n_burnin,
+      n.thin = n_thin,
+      n.chains = n_chains
+    ))
+  # Convert back to 1000
+  data <- data %>%
+    dplyr::mutate(Age = Age * 1000, Age_err = Age_err * 1000)
+  data_grid <- data_grid %>%
+    dplyr::mutate(Age = Age * 1000)
+  # Output from mcmc & dataframes for plots
+  output_dataframes <- create_output_df(
+    noisy_model_run_output = noisy_model_run_output,
+    data_grid,
+    CI = 0.95
+  )
+  # Output with everything-------------
+  jags_output <- list(
+    #noisy_model_run_output = noisy_model_run_output,
+    #jags_data = jags_data,
+    #data = data,
+    #data_grid = data_grid,
+    output_dataframes = output_dataframes
+  )
+  
+  
+  return(jags_output)
+}
+
---
+++ cross_val_common_north_atlantic_analysis_south_north/main_cross_val_common_jags_south_north.R
@@ -0,0 +1,216 @@
+# Cross validation for new approach for RSL analysis of South North of North Atlantic
+
+# f(x,t) = c(t) + r_North(t)+ r_South(t) + gt + l(x,t) + Error
+# Model 1: Fit the global/common trend in just time
+# Model 2: Fit the regional component with fixed priors from previous run
+# Model 3: Strong priors on both common, regional and estimate the non-linear local component
+
+
+# Data from Andy's updated data
+# Clear workspace
+rm(list = ls())
+#---------Set working directory--------------
+setwd("/users/research/mupton/3. RSL_North_Atlantic/cross_val_common_north_atlantic_analysis_south_north")
+#----------Load packages--------------------
+library(devtools)
+library(reslr)
+library(tidyverse)
+library(ggplot2)
+remotes::install_github("tidyverse/purrr")
+library(geosphere) # distm
+library(readr) # read csv file
+library(xtable)
+# R functions-------
+source("R/plot_data.R") # Plotting separate north & south data & map plot
+source("R/internal_functions.R") # New basis functions for north & south data
+source("R/reslr_mcmc_fun.R") # New basis functions for north & south data
+source("R/cross_val_check_fun.R") # Cross validation
+source("R/reslr_load_fun.R") # updated reslr_load
+
+
+# Read in Andy's updated data:
+global_data <- read_csv("https://www.dropbox.com/s/kqv3o10dnnbx38w/CommonEra2023_new.csv?dl=1")
+colnames(global_data) <- c(
+  "Basin", "Region", "Site", "Reference", "Indicator",
+  "Latitude", "Longitude", "RSL", "RSL_err_upr", "RSL_err_lwr", "Age", "Age_2_err_upr", "Age_2_err_lwr"
+)
+# Updating the column names:
+global_df <- global_data %>%
+  dplyr::mutate(
+    RSL_err = (RSL_err_upr + RSL_err_lwr) / 2,
+    Age_err = ((Age_2_err_upr / 2) + (Age_2_err_lwr / 2)) / 2
+  ) %>%
+  # Only looking at Common era
+  dplyr::filter(Age >= 0)
+SL_df <- global_df %>%
+  dplyr::filter(Basin == "North Atlantic")
+
+# Removing Spain as it is problem with linear rate
+SL_df <- SL_df %>%
+  dplyr::filter(!Region == "Norway") %>%
+  dplyr::filter(!Site %in% c("Muskiz Estuary", "Plentzia Estuary")) %>%
+  dplyr::select(!c("RSL_err_upr", "RSL_err_lwr", "Age_2_err_upr", "Age_2_err_lwr", "Indicator"))
+
+# Remove index points
+SL_df <- SL_df %>%
+  dplyr::group_by(Site)%>%
+  dplyr::filter(dplyr::n()>2) %>%
+  # Combine Sand Point Russian & VC
+  mutate(Site = replace(Site, Site == "Sand Point Russian", "Sand Point")) %>%
+  mutate(Site = replace(Site, Site == "Sand Point VC", "Sand Point"))
+
+# North American sites only------------
+SL_df <- SL_df %>% filter(Region %in% c("Connecticut",
+                                        "Florida",
+                                        "Massachusetts",
+                                        "North Carolina","New Jersey",
+                                        "Magdelen Islands","Newfoundland",
+                                        "Quebec","Nova Scotia","Maine",
+                                        "New York","Rhode Island"))
+
+
+
+# We include gia rates before cross val--------------------
+SL_df_gia <- reslr_load_fun(
+  data = SL_df,
+  include_tide_gauge = TRUE,
+  include_linear_rate = TRUE,
+  # all_TG_1deg = TRUE,
+  TG_minimum_dist_proxy = TRUE,
+  # Including tg longer than 75 years in europe
+  list_preferred_TGs = c(
+    #"LAKE WORTH PIER",
+    "CHARLESTON I",
+    "FORT PULASKI",
+    "DAYTONA BEACH",
+    "WILMINGTON",
+    "SPRINGMAID PIER",
+    "NAPLES",
+    "FORT MYERS"
+  ),
+  prediction_grid_res = 30
+)
+
+# Filling in the NA columns for the dataset:
+input_gia_data <- 
+  SL_df_gia$data %>%
+  mutate(Basin = ifelse(is.na(Basin) == TRUE,"North Atlantic",Basin),
+         Region = ifelse(is.na(Region) == TRUE, "",Region),
+         Reference = ifelse(is.na(Reference) == TRUE, "PSMSL database", Reference),
+         SiteName = as.character(SiteName),
+        Site = ifelse(is.na(Site) == TRUE,SiteName,Site),
+        SiteName = as.factor(SiteName))
+
+
+# Running cross validation for just proxy records------------------
+# south_north_cross_val_list <- cross_val_check_fun(
+#   data = input_gia_data,
+#   data_grid = SL_df_gia$data_grid,
+#   n_fold = 10,
+#   spline_nseg_c = 20,
+#   spline_nseg_t = 4,
+#   spline_nseg_st = 6,
+#   n_iterations = 8000,
+#   n_burnin = 1000,
+#   n_thin = 4,
+#   n_chains = 2,
+#   seed = 38233,
+#   prediction_grid_res = 30
+# )
+# saveRDS(south_north_cross_val_list, file = "reslr_outputs/cross_val_sn.rds")
+cross_val_sn <- readRDS("reslr_outputs/cross_val_sn.rds")
+cross_val_sn$ME_MAE_RSME_fold_site
+cross_val_sn$ME_MAE_RSME_overall
+true_pred_plt <- cross_val_sn$true_pred_plot_proxy
+ggsave(true_pred_plt,filename = "fig/true_pred_sn_proxy.pdf", width = 10, height = 6)
+cross_val_sn$total_coverage
+
+CV_model_df_proxy <- cross_val_sn$CV_model_df %>% 
+  filter(!data_type_id == "TideGaugeData") 
+
+CV_model_df_tg <- cross_val_sn$CV_model_df %>% 
+  filter(data_type_id == "TideGaugeData") %>% 
+  dplyr::mutate(SiteName = gsub(",\n ","",SiteName))
+
+CV_model_df<- rbind(CV_model_df_proxy,CV_model_df_tg)
+
+true_pred_plot_full <- ggplot2::ggplot(data = CV_model_df, ggplot2::aes(
+  x = true_RSL,
+  y = y_post_pred,
+  colour = "PI"
+)) +
+  ggplot2::geom_errorbar(
+    data = CV_model_df,
+    ggplot2::aes(
+      x = true_RSL,
+      ymin = lwr_PI,
+      ymax = upr_PI
+    ),
+    colour = "red3",
+    width = 0, alpha = 0.5
+  ) +
+  ggplot2::geom_point() +
+  ggplot2::geom_abline(
+    data = CV_model_df,
+    ggplot2::aes(intercept = 0, slope = 1, colour = "True = Predicted")
+  ) +
+  ggplot2::theme_bw() +
+  ggplot2::theme(
+    axis.title = ggplot2::element_text(size = 9, face = "bold"),
+    axis.text = ggplot2::element_text(size = 9),
+    strip.background = ggplot2::element_rect(fill = c("white")),
+    strip.text = ggplot2::element_text(size = 8),
+    legend.text = ggplot2::element_text(size = 7),
+    legend.title = ggplot2::element_blank(),
+    axis.text.x = ggplot2::element_text(size = 8),
+    axis.text.y = ggplot2::element_text(size = 8)
+  ) +
+  ggplot2::theme(legend.box = "horizontal", 
+                 legend.position = "bottom") +
+  ggplot2::labs(
+    x = "True Relative Sea Level (m)",
+    y = "Predicted Relative Sea Level (m)"
+  ) +
+  ggplot2::scale_colour_manual("",
+                               values = c(
+                                 c(
+                                   "PI" = "red3",
+                                   # "True = Predicted" = "black")
+                                   "True = Predicted" = "black"
+                                 )
+                               ),
+                               labels = c(
+                                 "PI" = paste0(unique(CV_model_df$CI), " Prediction Interval"),
+                                 "True = Predicted" = "True = Predicted"
+                               )
+  ) +
+  ggplot2::facet_wrap(~SiteName, scales = "free") +
+  ggplot2::guides(
+    colour = ggplot2::guide_legend(override.aes = list(
+      linetype = c(1, 1),
+      shape = c(NA, NA),
+      size = 2
+    ))
+  )+
+  theme(
+    panel.spacing = unit(0,'lines')
+  )+
+  theme(legend.position = c(1, 0),
+        legend.justification = c(1, 0),
+        legend.title=element_blank(),
+        legend.margin=margin(c(1,5,5,5)))
+true_pred_plot_full
+ggsave(true_pred_plot_full,filename = "fig/true_pred_sn.pdf", width = 10, height = 6)
+
+# Prediction Interval size
+prediction_interval_size <- CV_model_df %>%
+  dplyr::group_by(SiteName) %>%
+  dplyr::reframe(PI_width = abs(unique(mean(upr_PI - lwr_PI))),
+                 coverage_by_site =
+                   unique(length(which(obs_in_PI == "TRUE")) / dplyr::n()),
+                 RSME = unique(sqrt((sum(true_RSL - pred_RSL)^2) / dplyr::n())))
+print(xtable(prediction_interval_size,type = "latex",digits = 4),
+      file = "reslr_outputs/coverage_by_site_RSME.tex",
+      include.rownames=FALSE)
+
+
